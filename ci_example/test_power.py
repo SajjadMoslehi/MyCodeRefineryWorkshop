8327c96763302b52af60d7dc6ce6a74c75e5d1f7
Sajjad Moslehi <s.moslehi@tudelft.nl>
2025-03-19 16:24:05 +0100

    Add some comments

--- ci_example/test_power.py
+++ ci_example/test_power.py
@@ -1,3 +1,6 @@
+# The file name must start from test_ so that the CI can recognize the test file to run.
+# The function name must start from test_ so that the CI cna recognize the test file to run.
+
 from file_power import power
 
 def test_power():
